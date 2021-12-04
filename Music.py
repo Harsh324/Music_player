477fb598a99e90772e76983c20d1d2f9c226966d
Harsh324 <harshravi324@gmail.com>
2021-12-04 09:52:45 +0530

    Play, Pause, Resume, Stop meyhod added

--- Music.py
+++ Music.py
@@ -1,8 +1,9 @@
+import audioplayer
 from pydub import AudioSegment
 from pydub.playback import play
 import fnmatch
 import os
-
+from audioplayer import AudioPlayer
 
 class Music:
 
@@ -10,7 +11,7 @@ class Music:
         self.Tracks = []
         self.Folders = []
         self.songsList = {}
-        
+        self.Audio = None
         self.__get_songs()
         self.__setList()
         
@@ -46,23 +47,54 @@ class Music:
             self.songsList[name] = path[:-1]
         
     
-
-    def play(self):
     
+
+    
+    def __play(self):
         val = 1
         for song in self.songsList.keys():
             print(val , ": ", song)
             val += 1
-            
         Num = int(input("Enter the Song Number to play : "))
         songsName = list(self.songsList)
-        
-        sound = AudioSegment.from_file(self.songsList[songsName[Num-1]], format="mp3")
-        play(sound)
+        #sound = AudioSegment.from_file(self.songsList[songsName[Num-1]], format="mp3")
+        #play(sound)
+
+        self.Audio = AudioPlayer(self.songsList[songsName[Num-1]])
+        self.Audio.play()
+        return self.Audio
+
+    def __Pause(self):
+        self.Audio.pause()
+        print("Audio is paused")
+        return self.Audio
+
+    def __Resume(self):
+        self.Audio.resume()
+        print("Audio is resumed")
+        return self.Audio
     
+    def __Stop(self):
+        self.Audio.stop()
+        print("Audio is stopped , Thank you.")
+        return self.Audio
+
+    
+    def Drive(self):
+        self.__play()
+        Num = int(input("Enter 1 to pause Audio : "))
+        if(Num == 1):
+            self.__Pause()
+        Num = int(input("Enter 2 to resume Audio : "))
+        if(Num == 2):
+            self.__Resume()
+        Num = int(input("Enter 3 to stop Audio : "))
+        if(Num == 3):
+            self.__Stop()
+
 
 
 
 
 music = Music()
-music.play()
+music.Drive()
