90592c5b88118994f3847375cffd131ed05a1b9d
Harsh-Upadhayay <harshupadhayay906@gmail.com>
2021-12-10 15:02:50 +0530

    initialized new webapp musicPlayer

---
+++ musicPlayer/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request , 'index.html')
+    #return HttpResponse("This is home page")
